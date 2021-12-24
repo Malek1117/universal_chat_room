f94c6a723314dd95c9894790fdc34f755248f2e1
Shahnawaz Malek <86410031+Malek1117@users.noreply.github.com>
2021-12-24 18:01:01 +0530

    socket initial setup done

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
+
+const socket_io = require('socket.io');
+const io = socket_io(server);
+
+app.use(express.static(path.join(__dirname,"public")));
 
 
 
-app.listen(3000, ()=>{
-    console.log("server start");
+io.on('connection', (socket) => {
+    console.log("connection with id:", socket.id);
+})
+
+server.listen(3000, ()=>{
+    console.log("server started");
 });
