647db6125ec9c200ed66b85ae9121b9a20ed2076
Shahnawaz Malek <86410031+Malek1117@users.noreply.github.com>
2021-12-25 18:35:31 +0530

    time bug fixed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,7 @@
 const chat_messages = document.getElementById("chat_area");
 const users_list = document.getElementById("users");
 const send_button = document.getElementById("add_message");
+const leave_button = document.getElementById("leave_room");
 const input = document.getElementById("message");
 
 const user = window.location.search.split("=")[1];
@@ -67,3 +68,11 @@ input.addEventListener("keypress",(e)=>{
         input.focus();
     }
 });
+
+leave_button.addEventListener("click",(e)=>{
+    const leave = confirm('Are you sure you want to leave the Global chat?');
+
+    if(leave){
+        window.location = "../index.html";
+    }
+})
--- utils/message_format.js
+++ utils/message_format.js
@@ -1,6 +1,8 @@
 function message_format(username, text){
     let today = new Date();
-    let time = today.getHours() + ":" + today.getMinutes();
+    let hour = today.getHours();
+    let minute = today.getMinutes()<=9?"0"+today.getMinutes():today.getMinutes();
+    let time = hour + ":" + minute;
 
     return{
         username,
